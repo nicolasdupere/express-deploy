981e46688f45f7411246ae4311842a12c3c734a9
Nicolas Dupere <nico@nico-world.com>
2018-05-01 15:25:09 -0400

    4e push

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 app.get('/', (req, res) => {
-    res.send("Hello world Lucas Dupere");
+    res.send("Hello world Lucas Dupere bienvue");
 })
 
 app.listen(3000);
